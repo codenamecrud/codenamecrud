6090692640ed7da484f679ce42b845727136bc7e
Freika <iamfrey@gmail.com>
2015-03-31 23:41:36 +0600

    Add pending specs for admin/courses controller and remove travis.

---
+++ spec/controllers/admin/courses_controller_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+describe CoursesController do
+
+  describe "GET #index" do
+    pending "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #show" do
+    pending "returns http success" do
+      get :show
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #edit" do
+    pending "returns http success" do
+      get :edit
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #new" do
+    pending "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "PATCH #update" do
+    pending "returns http success" do
+      get :update
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "DELETE #destroy" do
+    pending "returns http success" do
+      get :destroy
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "PUT #create" do
+    pending "returns http success" do
+      get :create
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
