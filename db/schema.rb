b130f1158b9c35926239b6e430a190f1de3c83a2
Freika <iamfrey@gmail.com>
2015-04-04 17:00:48 +0600

    Add text to main page and add some styles.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402172558) do
+ActiveRecord::Schema.define(version: 20150404103026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150402172558) do
     t.text     "you_learn"
     t.text     "you_build"
     t.string   "slug"
+    t.string   "teaser"
   end
 
   create_table "sections", force: :cascade do |t|
