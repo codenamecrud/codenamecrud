8483f2dc9e9ae72574ca9bdb6acc4db640ac08e0
Freika <iamfrey@gmail.com>
2015-04-05 14:14:18 +0600

    Add markdown and disqus.

---
+++ config/initializers/disqus.rb
@@ -0,0 +1,6 @@
+DisqusRails.setup do |config|
+  config::SHORT_NAME = "enfield-rookies"
+  config::SECRET_KEY = ENV['DISQUS_API_KEY']
+  config::PUBLIC_KEY = ENV['DISQUS_API_SECRET']
+  config::ACCESS_TOKEN = ENV['DISQUS_ACCESS_TOKEN']
+end
