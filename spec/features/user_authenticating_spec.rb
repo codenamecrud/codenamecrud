f56c8ce1e7801cbe1aaa7181c52c8dec376898d8
Freika <iamfrey@gmail.com>
2015-04-10 00:53:22 +0600

    Add test helpers and translate some devise pages.

--- spec/features/user_authenticating_spec.rb
+++ spec/features/user_authenticating_spec.rb
@@ -2,18 +2,28 @@ require 'rails_helper'
 
 feature 'User authenticating and unauthenticating' do
   given(:user) { build(:user) }
+  given(:saved_user) { create(:user) }
 
   scenario 'Registration' do
     visit root_path
 
     click_on 'Регистрация'
-    fill_in 'Name', with: user.name
+    fill_in 'Имя', with: user.name
     fill_in 'Email', with: user.email
     fill_in 'user_password', with: user.password
     fill_in 'user_password_confirmation', with: user.password
-    click_on 'Sign up'
+    click_on 'Зарегистрироваться'
 
     expect(page).to have_content "Привет, #{user.name}"
+  end
+
+  scenario 'Authenticating' do
+    sign_in_with(saved_user.email, saved_user.password)
+
+    visit root_path
+    click_on 'Выход'
+
+    expect(page).to have_content 'Выход из системы выполнен.'
   end
 
 end
