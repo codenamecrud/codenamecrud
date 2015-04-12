cb2752ee24c94565ec60d9e59ddbdc9fbf0bbbab
Freika <iamfrey@gmail.com>
2015-04-13 02:40:23 +0600

    Add ajax check-uncheck and css animation.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -6,12 +6,11 @@ class LessonsController < ApplicationController
 
   def mark_as_completed
     @lesson = Lesson.friendly.find(params[:id])
+    @course = @lesson.course
     if current_user.lessons.include? @lesson
       current_user.lessons.delete(@lesson)
     else
       current_user.lessons << @lesson
     end
-
-    redirect_to :back, notice: 'Lesson updated'
   end
 end
