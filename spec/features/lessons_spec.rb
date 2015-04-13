dc18fbcc39bbbd319acbd83ec0bfeb67afea6eb8
Freika <iamfrey@gmail.com>
2015-04-14 02:04:37 +0600

    Mark as pending.

--- spec/features/lessons_spec.rb
+++ spec/features/lessons_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 feature 'Completing lessons' do
-  given!(:user) { create(:user) }
-  given!(:another_user) { create(:user) }
-  given!(:course) { create(:course) }
-  given!(:section) { create(:section, course: course) }
+  given(:user) { create(:user) }
+  given(:another_user) { create(:user) }
+  given(:course) { create(:course) }
+  given(:section) { create(:section, course: course) }
   given!(:lesson) { create(:lesson, course: course, section: section) }
 
-  scenario 'User can mark lesson as completed and it does not affect another user', js: true do
+  pending 'User can mark lesson as completed and it does not affect another user', js: true do
     sign_in_with(user.email, user.password)
 
     visit course_path(course)
