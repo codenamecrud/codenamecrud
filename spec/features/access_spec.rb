ca3519b2b17cbd9699d7d779062d80e4b4417d55
Freika <iamfrey@gmail.com>
2015-04-15 23:38:40 +0600

    Add test to ensure admin have access to dashboard.

--- spec/features/access_spec.rb
+++ spec/features/access_spec.rb
@@ -5,9 +5,17 @@ feature 'Admin dashboard' do
 
   it 'User have not access to dashboard' do
     sign_in_with(user.email, user.password)
-
     visit admin_root_path
 
     expect(page).to have_content 'Доступ запрещен'
+  end
+
+  it 'Admin have access to dashboard' do
+    admin = create(:user, role: 'admin')
+    sign_in_with(admin.email, admin.password)
+    visit admin_root_path
+
+    expect(page).to have_content 'Админка'
+    expect(page).to have_content 'Быстрая статистика'
   end
 end
