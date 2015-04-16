1d5719cb87789a7ceff2d14599e5ef6ec505701c
Freika <iamfrey@gmail.com>
2015-04-17 02:56:00 +0600

    Remove duplicated lesson completions from log.

--- app/models/lesson_user.rb
+++ app/models/lesson_user.rb
@@ -4,10 +4,18 @@ class LessonUser < ActiveRecord::Base
   has_paper_trail
 
   after_create :record_object
+  before_save :check_completion
 
   private
 
   def record_object
     PaperTrail::Version.last.update(object: self.lesson_id)
+  end
+
+  def check_completion
+    user_lesson_doubles = PaperTrail::Version.where(whodunnit: self.user_id, object: self.lesson_id)
+    if user_lesson_doubles.count > 0
+      user_lesson_doubles.delete_all
+    end
   end
 end
