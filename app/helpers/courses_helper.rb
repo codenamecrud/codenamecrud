3e4e262004e0edae61d77c5db5013c4830c2fade
Freika <iamfrey@gmail.com>
2015-04-16 23:23:36 +0600

    Refactor link to course.

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,9 @@
 module CoursesHelper
+  def course_link(course, index)
+    if course.is_active?
+      link_to "#{index}: #{course.title}", course
+    else
+      "#{index}: #{course.title}"
+    end
+  end
 end
