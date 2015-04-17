f238e828e43a6b06d0324f3a139c51ab2014056e
Freika <iamfrey@gmail.com>
2015-04-18 01:18:35 +0600

    Add avatar to user activity.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def activity
-    @activity = PaperTrail::Version.where(item_type: 'LessonUser').paginate(page: params[:page], per_page: 20)
+    @activities = PaperTrail::Version.where(item_type: 'LessonUser').paginate(page: params[:page], per_page: 20)
   end
 end
