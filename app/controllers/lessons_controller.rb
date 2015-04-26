9c3fc972853556819a9317c280f295b6cca1892f
Freika <iamfrey@gmail.com>
2015-04-26 19:53:30 +0600

    Do some refactorings and remove precompiled assets.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,16 +1,20 @@
 class LessonsController < ApplicationController
+  before_action :set_lesson_and_course, only: [:show, :toggle_check]
   def show
-    @lesson = Lesson.friendly.find(params[:id])
-    @course = @lesson.course
   end
 
   def toggle_check
-    @lesson = Lesson.friendly.find(params[:id])
-    @course = @lesson.course
     if current_user.lessons.include? @lesson
       current_user.lessons.delete(@lesson)
     else
       current_user.lessons << @lesson
     end
+  end
+
+  private
+
+  def set_lesson_and_course
+    @lesson = Lesson.friendly.find(params[:id])
+    @course = @lesson.course
   end
 end
