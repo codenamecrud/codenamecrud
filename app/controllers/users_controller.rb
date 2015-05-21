a5c5e799616fa86ca1176363392694cb57401fca
Freika <iamfrey@gmail.com>
2015-05-22 00:52:37 +0600

    Reorder activities one more time, aww yeah, don't stop dancing.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
   def activity
     @activities = PaperTrail::Version.where(item_type: 'LessonUser')
-                                     .order(created_at: :asc)
+                                     .order(created_at: :desc)
                                      .paginate(page: params[:page], per_page: 20)
   end
 
   def show
     @user = User.find(params[:id])
     @activities = PaperTrail::Version.where(item_type: 'LessonUser', whodunnit: @user.id.to_s)
-    .paginate(page: params[:page], per_page: 20)
+                                     .order(created_at: :desc)
+                                     .paginate(page: params[:page], per_page: 20)
   end
 
 end
