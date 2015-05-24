09ffdeddf0f211e6bc4e7d393346e51789a52fc2
Freika <iamfrey@gmail.com>
2015-05-24 20:21:23 +0600

    Add contributors list to Contribute page.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,9 @@
 module PagesHelper
+  def show_contributor(contributor)
+    content_tag(:div, class: 'contributor') do
+      link_to contributor['html_url'], target: '_blank' do
+        image_tag(contributor['avatar_url'], size: '48x48', data: { toggle: 'tooltip', placement: 'top' }, title: contributor['login'])
+      end
+    end
+  end
 end
