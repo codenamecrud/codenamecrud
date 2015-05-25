947071d8981d5c371e34a7e1e658dc1559389d87
Sergey Novikov <novikov359@gmail.com>
2015-05-25 12:23:17 +0300

    Fixed papertrail rake error

--- config/initializers/paper_trail.rb
+++ config/initializers/paper_trail.rb
@@ -1,4 +1,6 @@
-PaperTrail::Rails::Engine.eager_load! unless Rails.env.development?
+if !Rails.env.development? || $0 =~ /rake$/
+  PaperTrail::Rails::Engine.eager_load!
+end
 
 module PaperTrail
   class Version < ActiveRecord::Base
