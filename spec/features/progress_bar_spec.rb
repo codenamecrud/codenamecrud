8137861b4da836d5503a501e2955981051a85838
RuslanVikhor <tokvbok@yahoo.co.uk>
2015-05-31 20:47:43 +0100

    erb to slim + css to sass

---
+++ spec/features/progress_bar_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature 'Navigating between lessons' do
+  given(:user) { create(:user) }
+  given(:course) { create(:course) }
+
+  given(:section1) { create(:section, course: course) }
+
+  given!(:lesson1) { create(:lesson, course: course, section: section1) }
+  given!(:lesson2) { create(:lesson, course: course, section: section1) }
+  given!(:lesson3) { create(:lesson, course: course, section: section1) }
+
+
+
+  it 'displys progress bar' do
+    Capybara.ignore_hidden_elements = false
+    sign_in_with(user.name, user.password)
+    visit course_path(course)
+
+    within ('div.col-sm-8.col-sm-offset-2') do
+      find('div.lesson')
+      find('div.row.bs-wizard')
+    end
+    expect(page).to have_css('div.progress-bar')
+  end
+end
