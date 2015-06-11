afecf603c63e14c59ceaa010d8984ef67c709578
Freika <iamfrey@gmail.com>
2015-06-11 21:47:18 +0600

    Use redcarpet instead of kramdown to parse markdown.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,20 @@
 module ApplicationHelper
+  # def markdown(text)
+  #   Kramdown::Document.new(text, coderay_line_numbers: nil).to_html.html_safe
+  # end
+
   def markdown(text)
-    Kramdown::Document.new(text, coderay_line_numbers: nil).to_html.html_safe
+    renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true)
+    options = {
+        autolink: true,
+        no_intra_emphasis: true,
+        fenced_code_blocks: true,
+        lax_spacing: true,
+        strikethrough: true,
+        superscript: true,
+        space_after_headers: true
+    }
+    Redcarpet::Markdown.new(renderer, options).render(text).html_safe
   end
 
   def breadcrumbs
