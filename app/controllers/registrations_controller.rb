addc5c244c959a167c6bc4eee75df3b0bed8fd60
andreyleonov <andrey.leonov@gmail.com>
2015-08-27 17:35:22 +0800

    Refactored code for user password change

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,14 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
   def update
     @user = current_user
 
-    successfully_updated = if needs_password?(@user, params)
-      @user.update_with_password(safe_params)
-    else
-      params[:user].delete(:current_password)
-      @user.update_without_password(safe_params)
-    end
-
-    if successfully_updated
+    if update_password
       set_flash_message :notice, :updated
       redirect_to after_update_path_for(@user)
     else
@@ -22,12 +15,18 @@ class RegistrationsController < Devise::RegistrationsController
     current_user.clear_provider!
     redirect_to :back
   end
-
-  def needs_password?(user, params) 
+  
+  private
+  
+  def needs_password?
     params[:user][:password].present?
   end
-
-  private
+  
+  def update_password
+    return @user.update_with_password(safe_params) if needs_password?
+    params[:user].delete(:current_password)
+    @user.update_without_password(safe_params)
+  end
 
   def safe_params
     devise_parameter_sanitizer.sanitize(:account_update)
