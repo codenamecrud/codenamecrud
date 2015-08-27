2ba38bc4056bcb71b086c870397874ffdf686bab
andreyleonov <andrey.leonov@gmail.com>
2015-08-27 13:19:07 +0800

    Fixed: change password function did not work properly

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,17 +3,28 @@ class RegistrationsController < Devise::RegistrationsController
   def update
     @user = current_user
 
-    if @user.update_without_password(safe_params)
+    successfully_updated = if needs_password?(@user, params)
+      @user.update_with_password(safe_params)
+    else
+      params[:user].delete(:current_password)
+      @user.update_without_password(safe_params)
+    end
+
+    if successfully_updated
       set_flash_message :notice, :updated
       redirect_to after_update_path_for(@user)
     else
-      render 'edit'
+      render "edit"
     end
   end
 
   def cancel_oauth
     current_user.clear_provider!
     redirect_to :back
+  end
+
+  def needs_password?(user, params) 
+    params[:user][:password].present?
   end
 
   private
