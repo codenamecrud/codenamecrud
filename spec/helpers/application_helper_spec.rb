9c7a013e60d67be6fa7de21819ca57722aec997d
dipiash <dipiash@users.noreply.github.com>
2015-09-02 18:53:13 +0300

    Added test for new method 'next_lesson_path'

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -10,4 +10,10 @@ describe ApplicationHelper do
       expect(helper.breadcrumbs).to eq([link_to('Список курсов', pages_curriculum_path), link_to(@lesson.course.title, course_path(@lesson.course)), link_to(@lesson.section.title, course_path(@lesson.course, anchor: "section-#{@lesson.section.slug}")), @lesson.title])
     end
   end
+
+  describe '#next_lesson_path' do
+    it 'returns blank' do
+      expect(helper.next_lesson_path).to be_blank
+    end
+  end
 end
