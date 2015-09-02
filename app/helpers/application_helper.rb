4a489518a2d74e477cc9bb4aa98bd1a58617cc7f
dipiash <dipiash@users.noreply.github.com>
2015-09-02 18:14:56 +0300

    Fixed. Course we can take from lesson

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -46,7 +46,7 @@ module ApplicationHelper
     begin
       last_lesson_id = LessonUser.maximum(:lesson_id, user_id: current_user.id) + 1
       lesson = Lesson.find_by(id: last_lesson_id)
-      course = Course.find_by(position: lesson.course_id)
+      course = lesson.course
       link_to_last_lesson =  "#{course.slug}/#{lesson.slug}"
     rescue
       ""
