58a49e1954b04d0b8a3f2dbebda253d594ad95f2
dipiash <dipiash@users.noreply.github.com>
2015-09-05 14:24:28 +0300

    Removed test text from link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -52,7 +52,7 @@ module ApplicationHelper
       last_lesson_id = ids_lesson.max + 1
       lesson = Lesson.find_by(id: last_lesson_id)
       course = lesson.course
-      link_to_last_lesson =  "#{course.slug}/#{lesson.slug}/#{get_id_to_lock_lesson}"
+      link_to_last_lesson =  "#{course.slug}/#{lesson.slug}"
     rescue
       ""
     end
