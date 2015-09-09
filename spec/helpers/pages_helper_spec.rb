3f878b1b94ee0b22c7003262a39888237f63437e
dipiash <dipiash@users.noreply.github.com>
2015-09-09 13:38:39 +0300

    Fixed style

--- spec/helpers/pages_helper_spec.rb
+++ spec/helpers/pages_helper_spec.rb
@@ -20,11 +20,12 @@ describe PagesHelper do
         expect(item).to have_css('div.contributor > a')
         expect(item).to have_css('div.contributor > a > img')
 
-        expect(item).to eq(content_tag(:div, class: 'contributor') do
-                              link_to contributor['html_url'], target: '_blank' do
-                                image_tag(contributor['avatar_url'], size: '48x48', data: { toggle: 'tooltip', placement: 'top' }, title: contributor['login'])
-                              end
-                            end)
+        expect(item).to eq(
+          content_tag(:div, class: 'contributor') do
+            link_to contributor['html_url'], target: '_blank' do
+                image_tag(contributor['avatar_url'], size: '48x48', data: { toggle: 'tooltip', placement: 'top' }, title: contributor['login'])
+            end
+          end)
       end
 
     end
