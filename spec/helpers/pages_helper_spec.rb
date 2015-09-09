aa33a048a6a2b7d977094deee99bebc1841cf905
dipiash <dipiash@users.noreply.github.com>
2015-09-09 13:10:29 +0300

    Added test for pages_helper

---
+++ spec/helpers/pages_helper_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe PagesHelper do
+  describe '#show_contributor' do
+    it 'page should have div contributor' do
+      visit pages_contribute_path
+      expect(page).to have_css('div.contributor')
+      expect(page).to have_css('div.contributor > a')
+      expect(page).to have_css('div.contributor > a > img')
+    end
+
+    it 'tested all item for curriculum' do
+      contribute
+
+      @curriculum.each do |contributor|
+
+        item = show_contributor(contributor)
+
+        expect(item).to have_css('div.contributor')
+        expect(item).to have_css('div.contributor > a')
+        expect(item).to have_css('div.contributor > a > img')
+
+        expect(item).to eq(content_tag(:div, class: 'contributor') do
+                              link_to contributor['html_url'], target: '_blank' do
+                                image_tag(contributor['avatar_url'], size: '48x48', data: { toggle: 'tooltip', placement: 'top' }, title: contributor['login'])
+                              end
+                            end)
+      end
+
+    end
+  end
+
+end
