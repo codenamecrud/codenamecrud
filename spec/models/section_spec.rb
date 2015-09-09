ead60b1ddd5f504a5021dacfff0e47a5d08b300f
dipiash <dipiash@users.noreply.github.com>
2015-09-09 22:06:05 +0300

    Writed a new tests

--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -7,4 +7,22 @@ describe Section do
   it { should validate_presence_of(:slug) }
   it { should validate_presence_of(:description) }
 
+  it "#next_lesson_in_the_section should have next lesson" do
+    lesson1 = FactoryGirl.create(:lesson, id: 1, course_id: 1, section_id: 1)
+    lesson2 = FactoryGirl.create(:lesson, id: 2, course_id: 1, section_id: 1)
+    course = FactoryGirl.create(:course, id: 1)
+    section = FactoryGirl.create(:section, id: 1, course: course)
+
+    expect(section.next_lesson_in_the_section(lesson1)).to eq(lesson2)
+  end
+
+  it "#next_lesson_in_the_section should not have next lesson" do
+    lesson1 = FactoryGirl.create(:lesson, id: 1, course_id: 1, section_id: 1)
+    lesson2 = FactoryGirl.create(:lesson, id: 2, course_id: 1, section_id: 2)
+    course = FactoryGirl.create(:course, id: 1)
+    section = FactoryGirl.create(:section, id: 1, course: course)
+
+    expect(section.next_lesson_in_the_section(lesson1)).to eq(nil)
+  end
+
 end
