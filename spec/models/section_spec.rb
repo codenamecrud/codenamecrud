abaaa7073d4188f632f074221e2871b31e6f3400
dipiash <dipiash@users.noreply.github.com>
2015-09-10 08:26:46 +0300

    Refactored tests

--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -8,19 +8,20 @@ describe Section do
   it { should validate_presence_of(:description) }
 
   it "#next_lesson_in_the_section should have next lesson" do
-    lesson1 = FactoryGirl.create(:lesson, id: 1, course_id: 1, section_id: 1)
-    lesson2 = FactoryGirl.create(:lesson, id: 2, course_id: 1, section_id: 1)
     course = FactoryGirl.create(:course, id: 1)
     section = FactoryGirl.create(:section, id: 1, course: course)
+    lesson1 = FactoryGirl.create(:lesson, id: 1, course: course, section: section)
+    lesson2 = FactoryGirl.create(:lesson, id: 2, course: course, section: section)
 
     expect(section.next_lesson_in_the_section(lesson1)).to eq(lesson2)
   end
 
   it "#next_lesson_in_the_section should not have next lesson" do
-    lesson1 = FactoryGirl.create(:lesson, id: 1, course_id: 1, section_id: 1)
-    lesson2 = FactoryGirl.create(:lesson, id: 2, course_id: 1, section_id: 2)
     course = FactoryGirl.create(:course, id: 1)
     section = FactoryGirl.create(:section, id: 1, course: course)
+    section1 = FactoryGirl.create(:section, id: 2, course: course)
+    lesson1 = FactoryGirl.create(:lesson, id: 1, course: course, section: section)
+    lesson2 = FactoryGirl.create(:lesson, id: 2, course: course, section: section1)
 
     expect(section.next_lesson_in_the_section(lesson1)).to eq(nil)
   end
