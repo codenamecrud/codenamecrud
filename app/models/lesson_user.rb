3fc4a0a21ce74945bc157b5648ec83871ce4ded2
Vitaliy Gambala <gambala.rus@gmail.com>
2016-01-20 16:27:21 +0300

    Remove redundant scope

--- app/models/lesson_user.rb
+++ app/models/lesson_user.rb
@@ -6,8 +6,6 @@ class LessonUser < ActiveRecord::Base
   after_create :record_object
   before_save :check_completion
 
-  scope :lesson_completions, -> { where(item_type: 'LessonUser') }
-
   private
 
   def record_object
