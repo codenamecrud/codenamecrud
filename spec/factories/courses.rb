beec5c00b1cc02cdbc715bcc109f3b813ba001d6
Vitaliy Gambala <gambala.rus@gmail.com>
2016-01-20 15:28:25 +0300

    Refactor word arrays

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -4,8 +4,8 @@ FactoryGirl.define do
     sequence(:slug)  { |n| "course_ruby-slug-#{n}" }
     description "MyText"
     short_description 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum consequatur necessitatibus recusandae quo dolor tenetur consectetur quas maxime, adipisci sequi, est ad nam! Quasi est soluta modi sit laboriosam voluptatem.'
-    you_learn ['test', 'test', 'test']
-    you_build ['test', 'test', 'test']
+    you_learn %w(test test test)
+    you_build %w(test test test)
     is_active false
   end
 
