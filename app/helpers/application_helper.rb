06d0b298a92cf8de5ede300abdcb2aeaf486d723
Freika <iamfrey@gmail.com>
2016-01-25 19:46:57 +0600

    Remove lesson lock.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -57,13 +57,4 @@ module ApplicationHelper
       ""
     end
   end
-
-  def get_id_to_lock_lesson
-    begin
-      id_lock = LessonUser.find_by_sql(["SELECT lesson_id FROM lesson_users WHERE user_id = ? ORDER BY lesson_id DESC", current_user.id])
-      { id_lesson_lock: id_lock[0][:lesson_id] }
-    rescue
-      { id_lesson_lock: 0 }
-    end
-  end
 end
