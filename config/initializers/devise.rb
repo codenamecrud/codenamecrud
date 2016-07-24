3823974bb60a90362e9d15773f07313e4905e911
Freika <iamfrey@gmail.com>
2016-07-24 14:31:47 +0700

    Update devise timeout.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,6 +14,7 @@ Devise.setup do |config|
   config.expire_all_remember_me_on_sign_out = true
   config.password_length = 8..128
   config.reset_password_within = 6.hours
+  config.timeout_in = 1.hour
   config.sign_out_via = :delete
   config.secret_key = 'f43cc134134eb9c9097939379556ef5ee4258a3a038b9569b84d2ea05a8f66d590e3d6dcc51c88c72d8dded8bac16a9ed96bf2a02b9b18da3a027af51a9b3278'
 end
